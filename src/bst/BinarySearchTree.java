b2ddfa59379113ce81ed6a093dbeb2624f205e04
bruski <292803281@qq.com>
2020-04-22 23:58:45 +0800

    feat(bst): 完成二分搜索树的删除

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package bst;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BinarySearchTree<E extends Comparable<E>> {
     public static void main(String[] args) {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
@@ -7,14 +11,31 @@ public class BinarySearchTree<E extends Comparable<E>> {
         for (int num: nums) {
             bst.add(num);
         }
-        bst.preOrder();
-        System.out.println();
+        System.out.println(bst);
+        System.out.println(bst.getSize());
 
-        bst.inOrder();
-        System.out.println();
+//        bst.removeMin();
+//        System.out.println(bst);
+//        System.out.println(bst.getSize());
 
-        bst.postOrder();
-        System.out.println();
+        bst.remove(3);
+        System.out.println(bst);
+        System.out.println(bst.getSize());
+
+
+
+//        bst.preOrder();
+//        System.out.println();
+//
+//        bst.preOrderNR();
+
+//        bst.levelOrder();
+
+//        bst.inOrder();
+//        System.out.println();
+//
+//        bst.postOrder();
+//        System.out.println();
 //        System.out.println(bst);
     }
 
@@ -55,6 +76,175 @@ public class BinarySearchTree<E extends Comparable<E>> {
     }
 
     /**
+     * 向以node为根的二分搜索树，插入元素E
+     * 返回插入新节点后的二分搜索数的根
+     */
+    private Node recursiveAdd(Node node, E e) {
+        // 最小问题的解法:当前节点为空，直接创建
+        if (node == null) {
+            size ++;
+            return new Node(e);
+        }
+
+        // 否则，递归
+        if (e.compareTo(node.e) < 0) {
+            // 小于当前节点，递归左子树
+            node.left = recursiveAdd(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = recursiveAdd(node.right, e);
+        }
+        // 插入值与当前根节点相等，什么都不做
+        return node;
+    }
+
+    public E getMin() {
+        return getMinByRecursive(root).e;
+    }
+
+    private Node getMinByRecursive(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.left == null) {
+            return node;
+        }
+        return getMinByRecursive(node.left);
+    }
+
+    /**
+     * 删除二叉搜索树中最小节点，并返回最小值
+     * @return E
+     */
+    public E removeMin() {
+        E ret = getMin();
+        removeMinByRecursive(root);
+        return ret;
+    }
+
+    private Node removeMinByRecursive(Node node) {
+        if (node == null) {
+            return null;
+        }
+        // 遍历到最左叶节点即最小值，将其节点的右子树赋值给其上一节点
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size --;
+            return rightNode;
+        }
+        // 否则,递归删除左子树
+        node.left = removeMinByRecursive(node.left);
+        return node;
+    }
+
+    public E getMax() {
+        return getMaxByRecursive(root).e;
+    }
+
+    private Node getMaxByRecursive(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.right == null) {
+            return node;
+        }
+        return getMaxByRecursive(node.right);
+    }
+
+    /**
+     * 删除二叉搜索树中最大节点，并返回最大值
+     * @return E
+     */
+    public E removeMax() {
+        E ret = getMax();
+        removeMaxByRecursive(root);
+        return ret;
+    }
+
+    private Node removeMaxByRecursive(Node node) {
+        if (node == null) {
+            return null;
+        }
+        // 遍历到最左叶节点即最小值，将其节点的右子树赋值给其上一节点
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size --;
+            return leftNode;
+        }
+        // 否则,递归删除左子树
+        node.right = removeMaxByRecursive(node.right);
+        return node;
+    }
+
+    public Node remove(E e) {
+        return removeByRecursive(root, e);
+    }
+
+    private Node removeByRecursive(Node node, E e) {
+        if (node == null) {
+            return null;
+        }
+        if (e.compareTo(node.e) < 0) {
+            node.left = removeByRecursive(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = removeByRecursive(node.right, e);
+            return node;
+        } else {
+            // node.e == e
+            // 删除分左子树为空、右子树为空、左右子树不为空的情况
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+            // 左右子树都不为空，找到当前节点的右子树最小值节点作为后继节点
+            // 将当前节点的左子树赋给后继节点
+            // 将当前节点的删除了该后继节点的右子树，赋给该后继节点
+//            return processBySuccessor(node);
+            return processByFront(node);
+        }
+    }
+
+    /**
+     * 通过后继节点调整树
+     * @param node
+     * @return
+     */
+    private Node processByFront(Node node) {
+        Node front = getMaxByRecursive(node.left);
+        front.left = removeMaxByRecursive(node.left); // 此处发生了一次 size --
+        front.right = node.right;
+
+        // 删除当前节点
+        node.left = node.right = null;
+        return front;
+    }
+
+    /**
+     * 通过后继节点调整树
+     * @param node
+     * @return
+     */
+    private Node processBySuccessor(Node node) {
+        Node successor = getMinByRecursive(node.right);
+        successor.right = removeMinByRecursive(node.right); // 此处发生了一次 size --
+        successor.left = node.left;
+
+        // 删除当前节点
+        node.left = node.right = null;
+        return successor;
+    }
+
+    /**
      * 查找目标元素是否包含元素e
      * @param e E
      * @return boolean
@@ -78,6 +268,31 @@ public class BinarySearchTree<E extends Comparable<E>> {
         recursivePreOrder(node.left); // 遍历左子树
         recursivePreOrder(node.right); // 遍历右子树
     }
+
+    /**
+     * 前序遍历的非递归实现
+     */
+    public void preOrderNR() {
+        if (root == null) {
+            return;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            System.out.println(node.e);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+    }
+
 
     /**
      * 中序遍历
@@ -113,6 +328,30 @@ public class BinarySearchTree<E extends Comparable<E>> {
         System.out.println(node.e); // 在中间访问节点
     }
 
+    /**
+     * 层序遍历
+     * 使用队列实现
+     */
+    public void levelOrder() {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            Node node = q.remove();
+            System.out.println(node.e);
+
+            if (node.left != null) {
+                q.add(node.left);
+            }
+            if (node.right != null) {
+                q.add(node.right);
+            }
+        }
+    }
+
     private boolean recursiveContains(Node node, E e) {
         if (node == null) {
             return false;
@@ -124,27 +363,6 @@ public class BinarySearchTree<E extends Comparable<E>> {
         } else { // e.compareTo(node.e) > 0
             return recursiveContains(node.right, e);
         }
-    }
-
-    /**
-     * 向以node为根的二分搜索树，插入元素E
-     * 返回插入新节点后的二分搜索数的根
-     */
-    private Node recursiveAdd(Node node, E e) {
-        // 最小问题的解法:当前节点为空，直接创建
-        if (node == null) {
-            return new Node(e);
-        }
-
-        // 否则，递归
-        if (e.compareTo(node.e) < 0) {
-            // 小于当前节点，递归左子树
-            node.left = recursiveAdd(node.left, e);
-        } else if (e.compareTo(node.e) > 0) {
-            node.right = recursiveAdd(node.right, e);
-        }
-        // 插入值与当前根节点相等，什么都不做
-        return node;
     }
 
     /**
